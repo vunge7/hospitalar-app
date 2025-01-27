29f40f4d776cf2f946eb4399968ac2a6755c04fe
vunge7 <55225676+vunge7@users.noreply.github.com>
2025-01-27 16:56:38 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route, useNavigate, BrowserRouter } from 'react-router-dom';
 import AuthProvider from '../src/contexts/auth';
 import Login from '../src/pages/Login';
 import RoutesApp from './routes';
+//Alterado no github
+//Alterado no VSCODE
 
 function App() {
     return (
